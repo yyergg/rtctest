63790ecd9498f4a767ca076ea7624d493ec8381e
yyergg <yyergg@gmail.com>
2017-02-03 15:37:25 +0800

    LocalVideoTrack done.

--- app/src/main/java/com/example/yyergg/webrtctest/MainActivity.java
+++ app/src/main/java/com/example/yyergg/webrtctest/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.yyergg.webrtctest;
 
+import android.Manifest;
 import android.app.Activity;
 import android.media.projection.MediaProjection;
 import android.os.Environment;
 import android.os.ParcelFileDescriptor;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
 
 import org.webrtc.Camera1Enumerator;
 import org.webrtc.Camera2Enumerator;
@@ -19,11 +22,15 @@ import org.webrtc.Logging;
 import org.webrtc.MediaConstraints;
 import org.webrtc.MediaStream;
 import org.webrtc.PeerConnection;
+import org.webrtc.PeerConnectionFactory;
 import org.webrtc.ScreenCapturerAndroid;
 import org.webrtc.SessionDescription;
 import org.webrtc.StatsReport;
+import org.webrtc.SurfaceViewRenderer;
 import org.webrtc.VideoCapturer;
 import org.webrtc.VideoRenderer;
+import org.webrtc.VideoSource;
+import org.webrtc.VideoTrack;
 
 import java.io.File;
 import java.io.IOException;
@@ -37,15 +44,23 @@ import java.util.concurrent.ScheduledExecutorService;
 
 public class MainActivity extends AppCompatActivity {
     public static final String S_TAG = "MainActivity";
+    public static final String VIDEO_TRACK_ID = "Chasewind";
     public static final String EXTRA_CAPTURETOTEXTURE_ENABLED = "com.example.yyergg.webrtctest.CAPTURETOTEXTURE";
-
+    private static final String[] MANDATORY_PERMISSIONS = {"Manifest.permission.CAMERA"};
 
     private final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
 
+    private PeerConnectionFactory factory;
     private PeerConnectionParameters peerConnectionParameters;
     private MediaConstraints pcConstraints;
     private LinkedList<IceCandidate> queuedRemoteCandidates;
     private EglBase rootEglBase;
+    private SurfaceViewRenderer localRender;
+    private LinearLayout localLayout;
+    private PeerConnectionFactory.Options options;
+    private VideoCapturer capturer;
+    private VideoSource videoSource;
+    private VideoTrack localVideoTrack;
 
 
     @Override
@@ -55,198 +70,40 @@ public class MainActivity extends AppCompatActivity {
 
         pcConstraints = new MediaConstraints();
         rootEglBase = EglBase.create();
-    }
 
-    private void createPeerConnectionInternal(EglBase.Context renderEGLContext) {
-        Log.d(S_TAG, "PCConstraints: " + pcConstraints.toString());
-        queuedRemoteCandidates = new LinkedList<IceCandidate>();
+        localRender = (SurfaceViewRenderer) findViewById(R.id.local_video_view);
+        localRender.init(rootEglBase.getEglBaseContext(), null);
+        localRender.setZOrderMediaOverlay(true);
+        localRender.setEnableHardwareScaler(true);
 
-        if (videoCallEnabled) {
-            Log.d(S_TAG, "EGLContext: " + renderEGLContext);
-            factory.setVideoHwAccelerationOptions(renderEGLContext, renderEGLContext);
+        localLayout = (LinearLayout) findViewById(R.id.local_video_layout);
+
+        String[] permissions = new String[]{Manifest.permission.CAMERA};
+
+        ActivityCompat.requestPermissions(this, permissions, 0);
+
+
+
+        options = null;
+        PeerConnectionFactory.Options options = new PeerConnectionFactory.Options();
+        options.networkIgnoreMask = 0;
+        if (options != null) {
+            Log.d(S_TAG, "Factory networkIgnoreMask option: " + options.networkIgnoreMask);
         }
-
-        PeerConnection.RTCConfiguration rtcConfig =
-                new PeerConnection.RTCConfiguration(signalingParameters.iceServers);
-        // TCP candidates are only useful when connecting to a server that supports
-        // ICE-TCP.
-        rtcConfig.tcpCandidatePolicy = PeerConnection.TcpCandidatePolicy.DISABLED;
-        rtcConfig.bundlePolicy = PeerConnection.BundlePolicy.MAXBUNDLE;
-        rtcConfig.rtcpMuxPolicy = PeerConnection.RtcpMuxPolicy.REQUIRE;
-        rtcConfig.continualGatheringPolicy = PeerConnection.ContinualGatheringPolicy.GATHER_CONTINUALLY;
-        // Use ECDSA encryption.
-        rtcConfig.keyType = PeerConnection.KeyType.ECDSA;
-
-        peerConnection = factory.createPeerConnection(rtcConfig, pcConstraints, pcObserver);
-
-        if (dataChannelEnabled) {
-            DataChannel.Init init = new DataChannel.Init();
-            init.ordered = peerConnectionParameters.dataChannelParameters.ordered;
-            init.negotiated = peerConnectionParameters.dataChannelParameters.negotiated;
-            init.maxRetransmits = peerConnectionParameters.dataChannelParameters.maxRetransmits;
-            init.maxRetransmitTimeMs = peerConnectionParameters.dataChannelParameters.maxRetransmitTimeMs;
-            init.id = peerConnectionParameters.dataChannelParameters.id;
-            init.protocol = peerConnectionParameters.dataChannelParameters.protocol;
-            dataChannel = peerConnection.createDataChannel("ApprtcDemo data", init);
+        if (!PeerConnectionFactory.initializeAndroidGlobals(
+                this, true, true, true)) {
+            Log.d(S_TAG,"Failed to initializeAndroidGlobals");
         }
-        isInitiator = false;
+        factory = new PeerConnectionFactory(options);
 
-        // Set default WebRTC tracing and INFO libjingle logging.
-        // NOTE: this _must_ happen while |factory| is alive!
-        Logging.enableTracing("logcat:", EnumSet.of(Logging.TraceLevel.TRACE_DEFAULT));
-        Logging.enableLogToDebugOutput(Logging.Severity.LS_INFO);
+        capturer = createCameraCapturer(new Camera2Enumerator(this));
+        videoSource = factory.createVideoSource(capturer);
+        capturer.startCapture(1280, 720, 30);
 
-        mediaStream = factory.createLocalMediaStream("ARDAMS");
-        if (videoCallEnabled) {
-            mediaStream.addTrack(createVideoTrack(videoCapturer));
-        }
+        localVideoTrack = factory.createVideoTrack(VIDEO_TRACK_ID, videoSource);
+        localVideoTrack.setEnabled(true);
+        localVideoTrack.addRenderer(new VideoRenderer(localRender));
 
-        mediaStream.addTrack(createAudioTrack());
-        peerConnection.addStream(mediaStream);
-        if (videoCallEnabled) {
-            findVideoSender();
-        }
-
-        if (peerConnectionParameters.aecDump) {
-            try {
-                aecDumpFileDescriptor =
-                        ParcelFileDescriptor.open(new File(Environment.getExternalStorageDirectory().getPath()
-                                        + File.separator + "Download/audio.aecdump"),
-                                ParcelFileDescriptor.MODE_READ_WRITE | ParcelFileDescriptor.MODE_CREATE
-                                        | ParcelFileDescriptor.MODE_TRUNCATE);
-                factory.startAecDump(aecDumpFileDescriptor.getFd(), -1);
-            } catch (IOException e) {
-                Log.e(TAG, "Can not open aecdump file", e);
-            }
-        }
-
-        Log.d(TAG, "Peer connection created.");
-    }
-
-
-    private class PCObserver implements PeerConnection.Observer {
-        @Override
-        public void onIceCandidate(final IceCandidate candidate) {
-            executor.execute(new Runnable() {
-                @Override
-                public void run() {
-                    events.onIceCandidate(candidate);
-                }
-            });
-        }
-
-        @Override
-        public void onIceCandidatesRemoved(final IceCandidate[] candidates) {
-            executor.execute(new Runnable() {
-                @Override
-                public void run() {
-                    events.onIceCandidatesRemoved(candidates);
-                }
-            });
-        }
-
-        @Override
-        public void onSignalingChange(PeerConnection.SignalingState newState) {
-            Log.d(S_TAG, "SignalingState: " + newState);
-        }
-
-        @Override
-        public void onIceConnectionChange(final PeerConnection.IceConnectionState newState) {
-            executor.execute(new Runnable() {
-                @Override
-                public void run() {
-                    Log.d(S_TAG, "IceConnectionState: " + newState);
-                    if (newState == PeerConnection.IceConnectionState.CONNECTED) {
-                        events.onIceConnected();
-                    } else if (newState == PeerConnection.IceConnectionState.DISCONNECTED) {
-                        events.onIceDisconnected();
-                    } else if (newState == PeerConnection.IceConnectionState.FAILED) {
-                        reportError("ICE connection failed.");
-                    }
-                }
-            });
-        }
-
-        @Override
-        public void onIceGatheringChange(PeerConnection.IceGatheringState newState) {
-            Log.d(S_TAG, "IceGatheringState: " + newState);
-        }
-
-        @Override
-        public void onIceConnectionReceivingChange(boolean receiving) {
-            Log.d(S_TAG, "IceConnectionReceiving changed to " + receiving);
-        }
-
-        @Override
-        public void onAddStream(final MediaStream stream) {
-            executor.execute(new Runnable() {
-                @Override
-                public void run() {
-                    if (peerConnection == null || isError) {
-                        return;
-                    }
-                    if (stream.audioTracks.size() > 1 || stream.videoTracks.size() > 1) {
-                        reportError("Weird-looking stream: " + stream);
-                        return;
-                    }
-                    if (stream.videoTracks.size() == 1) {
-                        remoteVideoTrack = stream.videoTracks.get(0);
-                        remoteVideoTrack.setEnabled(renderVideo);
-                        for (VideoRenderer.Callbacks remoteRender : remoteRenders) {
-                            remoteVideoTrack.addRenderer(new VideoRenderer(remoteRender));
-                        }
-                    }
-                }
-            });
-        }
-
-        @Override
-        public void onRemoveStream(final MediaStream stream) {
-            executor.execute(new Runnable() {
-                @Override
-                public void run() {
-                    remoteVideoTrack = null;
-                }
-            });
-        }
-
-        @Override
-        public void onDataChannel(final DataChannel dc) {
-            Log.d(S_TAG, "New Data channel " + dc.label());
-
-            if (!dataChannelEnabled)
-                return;
-
-            dc.registerObserver(new DataChannel.Observer() {
-                public void onBufferedAmountChange(long previousAmount) {
-                    Log.d(S_TAG, "Data channel buffered amount changed: " + dc.label() + ": " + dc.state());
-                }
-
-                @Override
-                public void onStateChange() {
-                    Log.d(S_TAG, "Data channel state changed: " + dc.label() + ": " + dc.state());
-                }
-
-                @Override
-                public void onMessage(final DataChannel.Buffer buffer) {
-                    if (buffer.binary) {
-                        Log.d(S_TAG, "Received binary msg over " + dc);
-                        return;
-                    }
-                    ByteBuffer data = buffer.data;
-                    final byte[] bytes = new byte[data.capacity()];
-                    data.get(bytes);
-                    String strData = new String(bytes);
-                    Log.d(S_TAG, "Got msg: " + strData + " over " + dc);
-                }
-            });
-        }
-
-        @Override
-        public void onRenegotiationNeeded() {
-            // No need to do anything; AppRTC follows a pre-agreed-upon
-            // signaling/negotiation protocol.
-        }
     }
 
     /**
